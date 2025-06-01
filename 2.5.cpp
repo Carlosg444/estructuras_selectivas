9ada49fd0964adb68174b46cc509d4d3322a454e
Carlos Gallardo <cgallardos@unjbg.edu.pe>
2025-06-01 09:20:23 -0500

    Actualiza ejercicio 2 de estructuras selectivas:agrega una función para calcular el descuento y lo imprime

---
+++ 2.5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+float pagoFinal (float C){
+	if (C>1000){
+		return C*0.8;
+	}
+	return C;
+}
+float descuento (float C){
+	if (C>1000){
+		return C*0.2;
+	}
+	return 0;
+}
+
+int main (){
+	float C;
+	cout<<"Inserte el total de la compra: "<<endl;
+	cin>>C;
+	while (C<0){
+		cout<<"Error: el total de la compra no puede ser negativo, intentelo de nuevo"<<endl;
+		cout<<"Inserte el total de la compra: "<<endl;
+		cin>>C;
+	}
+	cout<<"El pago final es: "<<pagoFinal(C)<<endl;
+	cout<<"El descuento es: "<<descuento(C)<<endl;
+}
+
